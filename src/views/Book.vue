780048a3168699243df628c08b823b433e94f758
Andre Madarang <andre_madarang@hotmail.com>
2019-01-07 20:14:53 -0500

    Part 3 - Vue Apollo Queries

---
+++ src/views/Book.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="book">
+    <ApolloQuery :query="require('@/graphql/queries/Book.gql')" :variables="{ id: $route.params.id}">
+      <template slot-scope="{ result: { data, loading }, isLoading }">
+        <div v-if="isLoading">Loading...</div>
+        <div v-else>
+          <div>{{ data.book.title }}</div>
+          <div>{{ data.book.author }}</div>
+          <img :src="`http://booksql-laravel.test/img/${data.book.image}`" alt="book cover">
+        </div>
+      </template>
+    </ApolloQuery>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,113 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ApolloQuery :query="categoriesQuery">
+      <template slot-scope="{ result: { data, loading }, isLoading }">
+        <div v-if="isLoading">Loading...</div>
+        <div v-else>
+          <a href="#" class="link-margin" @click.prevent="selectCategory('all')">All</a>
+          <a href="#" class="link-margin" @click.prevent="selectCategory('featured')">Featured</a>
+          <a href="#" v-for="category of data.categories" :key="category.id" @click.prevent="selectCategory(category.id)" class="link-margin">
+            {{ category.id }}. {{ category.name }}
+          </a>
+        </div>
+      </template>
+    </ApolloQuery>
+
+    <div v-if="selectedCategory === 'all'">
+      <ApolloQuery :query="query">
+        <template slot-scope="{ result: { data, loading }, isLoading }">
+          <div v-if="isLoading">Loading...</div>
+          <div v-else>
+            <div v-for="book of data.books" :key="book.id">
+              <router-link :to="`/books/${book.id}`">
+                {{ book.id }}. {{ book.title }}
+              </router-link>
+              <div>{{ book.author }}</div>
+              <img :src="`http://booksql-laravel.test/img/${book.image}`" alt="cover image">
+            </div>
+          </div>
+        </template>
+      </ApolloQuery>
+    </div>
+
+    <div v-else-if="selectedCategory === 'featured'">
+      <ApolloQuery :query="query" :variables="{ featured: true }">
+        <template slot-scope="{ result: { data, loading }, isLoading }">
+          <div v-if="isLoading">Loading...</div>
+          <div v-else>
+            <div v-for="book of data.booksByFeatured" :key="book.id">
+              <router-link :to="`/books/${book.id}`">
+                {{ book.id }}. {{ book.title }}
+              </router-link>
+              <div>{{ book.author }}</div>
+              <img :src="`http://booksql-laravel.test/img/${book.image}`" alt="cover image">
+            </div>
+          </div>
+        </template>
+      </ApolloQuery>
+    </div>
+
+    <div v-else>
+      <ApolloQuery :query="query" :variables="{ id: selectedCategory }">
+        <template slot-scope="{ result: { data, loading }, isLoading }">
+          <div v-if="isLoading">Loading...</div>
+          <div v-else>
+            <div v-for="book of data.category.books" :key="book.id">
+              <router-link :to="`/books/${book.id}`">
+                {{ book.id }}. {{ book.title }}
+              </router-link>
+              <div>{{ book.author }}</div>
+              <img :src="`http://booksql-laravel.test/img/${book.image}`" alt="cover image">
+            </div>
+          </div>
+        </template>
+      </ApolloQuery>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import categoryQuery from '@/graphql/queries/Category.gql'
+import categoriesQuery from '@/graphql/queries/Categories.gql'
+import booksQuery from '@/graphql/queries/Books.gql'
+import booksFeaturedQuery from '@/graphql/queries/BooksFeatured.gql'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+      categoryQuery,
+      categoriesQuery,
+      booksQuery,
+      booksFeaturedQuery,
+      selectedCategory: 'all',
+      query: booksQuery,
+      categories: []
+    }
+  },
+  methods: {
+    selectCategory(category) {
+      if (category === 'all') {
+        this.query = booksQuery
+      } else if (category === 'featured') {
+        this.query = booksFeaturedQuery
+      } else {
+        this.query = categoryQuery
+      }
+
+      this.selectedCategory = category
+    }
   }
 }
 </script>
+
+<style>
+  .link-margin {
+    margin-right: 24px;
+  }
+</style>
+
